9d1d5b701631727477f20fbd6bbe8893a443bd78
viviScript <863628979@qq.com>
2018-09-12 20:57:44 +0800

    yz

--- common/js/common.js
+++ common/js/common.js
@@ -1,12 +1,4 @@
 
-$(function(){
-    $(window).scroll(function() {
-        console.log($(window).scrollTop())
-        if($(window).scrollTop() >= 600) {
-            $(".yz-header").addClass("yz-header-bg");
-        }else{
-            $('.yz-header').removeClass("yz-header-bg");
-        }
-    })
+function setUrl(url) {
+    window.location.href = url;
 }
-);
---
+++ static/js/home.js
@@ -0,0 +1,13 @@
+
+$(function(){
+    
+    $(window).scroll(function() {
+        console.log($(window).scrollTop())
+        if($(window).scrollTop() >= 600) {
+            $(".yz-header").addClass("yz-header-bg");
+        }else{
+            $('.yz-header').removeClass("yz-header-bg");
+        }
+    })
+}
+);

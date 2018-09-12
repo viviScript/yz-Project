a98a6f1d409c5c5e947bfca73d66157783c64950
viviScript <863628979@qq.com>
2018-09-13 03:43:52 +0800

    yz

--- common/js/common.js
+++ common/js/common.js
@@ -2,3 +2,11 @@
 function setUrl(url) {
     window.location.href = url;
 }
+
+$(function() {
+    $(".yz-user-img").hover(function (){  
+        $('.yz-user-details').show();
+    },function (){  
+        $('.yz-user-details').hide();
+    }); 
+})

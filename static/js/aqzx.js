e0bdcb8c0fbf2b758a728f0181ef716df4fe7863
viviScript <863628979@qq.com>
2018-09-14 14:24:57 +0800

    细节优化

---
+++ static/js/aqzx.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.block-btn').click(function () {
+        $('.yz-aqzx-dialog').css('display', 'block')
+    })
+    
+    $('.close').click(function () {
+        $('.yz-aqzx-dialog').css('display', 'none')
+    })
+})

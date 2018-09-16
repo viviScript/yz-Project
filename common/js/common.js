1b843cc0802ee6320beba0ea1e35bafddbb6f30b
viviScript <863628979@qq.com>
2018-09-16 23:29:12 +0800

    登陆注册

--- common/js/common.js
+++ common/js/common.js
@@ -1,12 +1,21 @@
-
 function setUrl(url) {
     window.location.href = url;
 }
 
-$(function() {
-    $(".yz-user-img").hover(function (){  
+function registration(el) {
+    $(el).siblings().removeClass("yz-registration-block");
+    $(el).addClass('yz-registration-block')
+}
+
+$(function () {
+    $(".yz-user-img").hover(function () {
         $('.yz-user-details').show();
-    },function (){  
+    }, function () {
         $('.yz-user-details').hide();
-    }); 
+    });
+
+    $('.yz-user-login').click(function () {
+        $('.yz-registration-login').addClass('yz-registration-block')
+    })
+
 })

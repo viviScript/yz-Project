fe8b2bd39189661cdb017c5b7e805e5c03fd9025
viviScript <863628979@qq.com>
2018-09-17 21:01:39 +0800

    认证技能

---
+++ static/js/rzjn.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.yz-rzjn-dredgeType-select .select-label-nav').hover(function() {
+        $('.yz-rzjn-dredgeType-select .select-label-nav .select-label-box').css('display', 'block')
+    },function() {
+        $('.yz-rzjn-dredgeType-select .select-label-nav .select-label-box').css('display', 'none')
+    })
+    $('.yz-rzjn-dredgeType-select .select-label-game').hover(function() {
+        $('.yz-rzjn-dredgeType-select .select-label-game .select-label-box').css('display', 'block')
+    },function() {
+        $('.yz-rzjn-dredgeType-select .select-label-game .select-label-box').css('display', 'none')
+    })
+})

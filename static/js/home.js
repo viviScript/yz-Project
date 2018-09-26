7ee6e4d7cb947c7ed4c4570b04f933eec7f913a5
webscript <863628979@qq.com>
2018-09-26 21:27:09 +0800

    1

--- static/js/home.js
+++ static/js/home.js
@@ -2,17 +2,20 @@
 $(function(){
     $('.yz-user-search').css('display', 'none')
     $(window).scroll(function() {
-        console.log($(window).scrollTop())
+        // console.log($(window).scrollTop())
         if($(window).scrollTop() >= 600) {
             $(".yz-header").addClass("yz-header-bg");
             $('.yz-user-search').css('display', 'block')
+            console.log($('.yz-user-headImg').attr('src'), 'aaa')
             if (!$('.yz-user-headImg').attr('src') || $('.yz-user-headImg').attr('src') == './static/img/icon_me.png') {
                 $('.yz-user-headImg').attr('src', './static/img/icon_touxiang2.png')
             }
         }else{
             $('.yz-header').removeClass("yz-header-bg");
             $('.yz-user-search').css('display', 'none')
-            $('.yz-user-headImg').attr('src', './static/img/icon_me.png') 
+            if (!$('.yz-user-headImg').attr('src') || $('.yz-user-headImg').attr('src') == './static/img/icon_me.png') {
+                $('.yz-user-headImg').attr('src', './static/img/icon_me.png')
+            }
         }
     })
 }

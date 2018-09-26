2a881d43ea79b1617127a4f1e51e7bffe84d7d27
webscript <863628979@qq.com>
2018-09-26 15:05:42 +0800

    图片点击功能

---
+++ static/js/xxpw.js
@@ -0,0 +1,35 @@
+$(function() {
+    /*
+    * 样式yz-xxpw-sort-active  添加橙色按钮背景 白色默认排序图标
+    * 样式yz-xxpw-sort-active-top  排序向上图标
+    * 样式yz-xxpw-sort-active-bottom  排序向下图标
+    * 删除这三个样式 默认白色按钮背景 灰色排序图标
+    * */
+    $('.yz-xxpw-sort-ul li').click(function() {
+        console.log(this)
+        $(this).siblings().removeClass("yz-xxpw-sort-active").removeClass('yz-xxpw-sort-active-top').removeClass('yz-xxpw-sort-active-bottom')
+        if ($(this).hasClass('yz-xxpw-sort-active')) {
+            if ($(this).hasClass('yz-xxpw-sort-active-top')) {
+                if ($(this).hasClass('yz-xxpw-sort-active-bottom')) {
+                    $(this).removeClass('yz-xxpw-sort-active-top')
+                    $(this).removeClass('yz-xxpw-sort-active-bottom')
+                } else {
+                    $(this).addClass('yz-xxpw-sort-active-bottom')
+                }
+            } else {
+                $(this).addClass('yz-xxpw-sort-active-top')
+            }
+
+        } else {
+            $(this).addClass('yz-xxpw-sort-active')
+            $(this).siblings().removeClass("yz-xxpw-sort-active").removeClass('yz-xxpw-sort-active-top').removeClass('yz-xxpw-sort-active-bottom')
+        }
+    })
+
+    $('.yz-xxpw-gameType-list li').click(function() {
+        $(this).addClass('yz-xxpw-gameType-list-active').siblings().removeClass("yz-xxpw-gameType-list-active")
+
+    })
+
+
+})
---
+++ static/js/zyds.js
@@ -0,0 +1,32 @@
+$(function() {
+    /*
+    * 样式yz-zyds-sort-active  添加橙色按钮背景 白色默认排序图标
+    * 样式yz-zyds-sort-active-top  排序向上图标
+    * 样式yz-zyds-sort-active-bottom  排序向下图标
+    * 删除这三个样式 默认白色按钮背景 灰色排序图标
+    * */
+    $('.yz-zyds-sort-ul li').click(function() {
+        console.log(this)
+        $(this).siblings().removeClass("yz-zyds-sort-active").removeClass('yz-zyds-sort-active-top').removeClass('yz-zyds-sort-active-bottom')
+        if ($(this).hasClass('yz-zyds-sort-active')) {
+            if ($(this).hasClass('yz-zyds-sort-active-top')) {
+                if ($(this).hasClass('yz-zyds-sort-active-bottom')) {
+                    $(this).removeClass('yz-zyds-sort-active-top')
+                    $(this).removeClass('yz-zyds-sort-active-bottom')
+                } else {
+                    $(this).addClass('yz-zyds-sort-active-bottom')
+                }
+            } else {
+                $(this).addClass('yz-zyds-sort-active-top')
+            }
+
+        } else {
+            $(this).addClass('yz-zyds-sort-active')
+            $(this).siblings().removeClass("yz-zyds-sort-active").removeClass('yz-zyds-sort-active-top').removeClass('yz-zyds-sort-active-bottom')
+        }
+    })
+    $('.yz-zyds-gameType-list li').click(function() {
+        $(this).addClass('yz-zyds-gameType-list-active').siblings().removeClass("yz-zyds-gameType-list-active")
+
+    })
+})
